b93fd393074e4fe7862d6d56c959388c30327742
Vladimir <Kolchuzhin@mail.ru>
2015-02-17 07:57:17 +0100

    Update initial.vhd

--- hAMSter_model/initial.vhd
+++ hAMSter_model/initial.vhd
@@ -4,6 +4,7 @@ constant mm_1:real:=  0.288058870532E-08; -- modal mass for mode 1
 constant dm_1:real:=  0.706010261432E-04; -- modal damping constant for mode 1
 constant mm_2:real:=  0.653087233995E-08; -- modal mass for mode 2
 constant dm_2:real:=  0.245685234375E-03; -- modal damping constant for mode 2
+
 -- eigenvector at master node 1
 constant fi1_1:real:=  0.997970744648    ; -- eigenvector mode 1 at master node 1
 constant fi1_2:real:=  0.999999999731    ; -- eigenvector mode 2 at master node 1
@@ -13,6 +14,7 @@ constant fi2_2:real:=  0.996567701520    ;
 -- eigenvector at master node 3
 constant fi3_1:real:= -0.803900764657    ;
 constant fi3_2:real:=  0.953682688426    ;
+
 -- element loads
 -- acceleration in z-direction 9.8e6 m/s**2
 constant el1_1:real:= -0.211089068271E-11; -- element load 1 mode 1
